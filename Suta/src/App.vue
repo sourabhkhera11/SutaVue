eeb4dee08719321f7f1e5c39f0087c94c1a35bb5
Sourabhkhera11 <sourabhkhera11@gmail.com>
2025-12-30 19:22:57 +0530

    Todays final

--- Suta/src/App.vue
+++ Suta/src/App.vue
@@ -1,20 +1,37 @@
 <script  lang="ts">
   import card from './components/card.vue';
   import { defineComponent } from 'vue';
-  
+  import SearchClient from "@gaspl/search-client";
   export default defineComponent({
     components:{
       card
+    },
+    data(){
+      return{
+        data: {} as object,
+        result:{} as object,
+      };
+    },
+    methods:{
+      async fetchData(){
+        var searchClient = new SearchClient("i23b7q1j4icuv9wzze35u44s", "JFH1L4JRGKLVFTQY4FQPXIFV");
+        searchClient.fields("discounted_price","fit","gender","image","isActive","isBestSeller","isSearchable","price","title","vendor","product_type","size","color","style").count(32);
+        this.data=await searchClient.search("shirt","QPI32C64SIDELBV7BSK5H5LD");
+        console.log(this.data.results);
+        this.result=this.data.results;
+        for (const element of this.result) {
+          console.log(element.title+" "+element.product_type+" "+element.price+" "+element.image.scr+" "+element.gender+" "+element.fit+" "+element.discounted_price+" "+element.style+" "+element.vendor);
+        }
+      }
+    },
+    mounted(){
+      this.fetchData();
     }
   })
 </script>
 
 <template>
-       <card/>
-       <card/>
-       <card/>
-       <card/>
-       <card/>       
+       <card v-for="(value,index) in result" :key="index" :user-data="value"/>       
 </template>
 
 <style scoped>
--- Suta/src/components/card.vue
+++ Suta/src/components/card.vue
@@ -2,7 +2,7 @@
     <div class="product lg:st-p-[10px] st-mt-0 st-px-[2px] st-mb-[8px] lg:st-px-[2.5px] lg:st-mb-[17.5px]  md:st-mb-[32px] lg:st-basis-[25%] st-basis-[50%] ">
         <div class="productcard-img  st-relative  ">
             <div class="badge st-z-[1] st-absolute st-top-0  st-leading-[23.1px] ">
-                    <span class="st-bg-[#000] st-text-[#fff] st-text-[11px] st-pt-1.6px st-pb-[1.6px] st-pl-[8px] st-pr-[8px] st-tracking-[1.98px]  st-flex" >NEW</span>
+                    <span class="st-bg-[#000] st-text-[#fff] st-text-[11px] st-pt-1.6px st-pb-[1.6px] st-pl-[8px] st-pr-[8px] st-tracking-[1.98px]  st-flex" >{{ userData.gender.toUpperCase() }}</span>
             </div>
             <div class="heartButton st-absolute st-right-0 st-z-2 st-flex  st-bg-[#cdc4c4] st-border-[1px] st-border-white st-rounded-[15px] st-flex st-align-middle st-justify-center st-pt-[6px] st-pr-[5px] st-pl-[4px] st-pb-[4px]  ">
             <svg class="" xmlns="http://www.w3.org/2000/svg" width="25px" height="20px" viewBox="0 0 24 24" fill="none">
@@ -10,28 +10,43 @@
             </svg>
             </div>
                 <a href="/products/jungle-rani" class="product-card__media  st-relative  st-group" >
-                <img class="st-cursor-pointer st-block" src="//suta.in/cdn/shop/files/DSC_8502.jpg?v=1759157417&amp;width=1536" alt="Jungle Rani" >
+                <img class="st-cursor-pointer st-block" :src="userData.image.src" alt="Jungle Rani" >
                 <img class="st-absolute st-inset-[0] st-opacity-[0] st-group-hover:opacity-100 "  src="https://cdn.shopify.com/s/files/1/0026/6544/7536/files/DSC9843_fa27a0ae-c1bc-44b5-af4d-6d69778e2df3_500x.jpg?v=1759212075" alt="2nd image">
                 </a>
-                <div class=" st-flex st-absolute st-bottom-[3px] st-bg-[#fff] st-left-[3px] st-rounded-[7px]
-                        st-py-[1px] st-px-[4px] st-text-[12px]"><p>4.7</p><svg aria-hidden="true" focusable="false" width="13" class="icon icon-star-rating" viewBox="0 0 12 11"><path d="M6 0v8.635L2.292 11 3.48 6.87 0 4.202l4.443-.187L6 0Zm0 0v8.635L9.708 11 8.52 6.87 12 4.202l-4.443-.187L6 0Z" fill="#ffb400"></path></svg><div>(17)</div></div>
+                <!-- <div class=" st-flex st-absolute st-bottom-[3px] st-bg-[#fff] st-left-[3px] st-rounded-[7px]
+                        st-py-[1px] st-px-[4px] st-text-[12px]"><p>4.7</p><svg aria-hidden="true" focusable="false" width="13" class="icon icon-star-rating" viewBox="0 0 12 11"><path d="M6 0v8.635L2.292 11 3.48 6.87 0 4.202l4.443-.187L6 0Zm0 0v8.635L9.708 11 8.52 6.87 12 4.202l-4.443-.187L6 0Z" fill="#ffb400"></path></svg><div>(17)</div></div> -->
                 </div>
                 <div class="productcardInfo st-py-[4px] st-px-[6px] st-justify-items-start st-border-[1px] st-border-black/13 st-rounded-bl-[5px] st-rounded-br-[5px] st-border-t-0 st-shadow-[1px_1px_3px_#00000021]">
                     <div class="stackcontent st-grid st-grid-rows-3 st-gap-y-[5px]">
-                        <a href="" class="st-text-[14px]">Jungle Rani</a>
+                        <a href="" class="st-text-[14px] st-text-center">{{ userData.title }}</a>
                         <div class="product-type st-flex st-gap-[5px] st-flex-wrap ">
-                            <div class="typecard st-text-[12px] st-tracking-[0.7px] st-px-[8px] st-w-fit st-border-[1px] st-border-[#8c8686] ">Saree</div>
-                                <div class="fabriccard st-text-[12px] st-tracking-[0.7px] st-px-[8px] st-w-fit st-border-[1px] st-border-[#8c8686] ">Viscose Blend</div>
+                            <div class="typecard st-text-[12px] st-tracking-[0.7px] st-px-[8px] st-w-fit st-h-fit st-border-[1px] st-border-[#8c8686] ">{{ userData.product_type }}</div>
+                                <div class="fabriccard st-text-[12px] st-tracking-[0.7px] st-px-[8px] st-w-fit st-h-fit st-border-[1px] st-border-[#8c8686] ">{{ userData.fit }}</div>
+                                <div class="fabriccard st-text-[12px] st-tracking-[0.7px] st-px-[8px] st-w-fit st-h-fit st-border-[1px] st-border-[#8c8686] ">{{ userData.style }}</div>
+                                <div class="fabriccard st-text-[12px] st-tracking-[0.7px] st-px-[8px] st-w-fit st-h-fit st-border-[1px] st-border-[#8c8686] ">{{ userData.vendor }}</div>
                                 </div>
-                                <div class="pricetag st-text-[12px]">
-                          ₹ 5,200.00
+                                <div class="prices st-flex st-gap-[15px]">
+                                    <div class="pricetag st-text-[12px] st-line-through st-text-red-600">
+                              ₹ {{ userData.price }}.00
+                                    </div>
+                                    <div class="pricetag st-text-[12px]">
+                              ₹ {{ userData.discounted_price }}.00
+                                    </div>
                                 </div>
                     </div>
                 </div>
     </div>
 </template>
 <script lang="ts">
-
+    import { defineComponent } from 'vue';
+    export default defineComponent({
+        props:{
+            userData: {
+            type: Object,
+            required: true
+        }
+    }
+    })
 </script>
 <style scoped>
     
